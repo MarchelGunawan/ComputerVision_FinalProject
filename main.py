75dbfa47ede348dde223da53c3ce58703aacc4b2
wawanmarchel <marchel.steya@student.president.ac.id>
2022-04-25 18:29:06 +0700

    First Commit

---
+++ main.py
@@ -0,0 +1,125 @@
+import cv2
+import numpy as np
+import HandTrackingModule as htm
+import time
+import autopy
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+
+wCam, hCam = 640, 480
+frameR = 100
+smoothening = 7
+
+
+pTime = 0
+plocX, plocY = 0, 0
+clocX, clocY = 0, 0
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+detector = htm.handDetector(maxHands=1)
+wScr, hScr = autopy.screen.size()
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+volRange = volume.GetVolumeRange()
+minVol = volRange[0]
+maxVol = volRange[1]
+vol = 0
+volBar = 400
+volPer = 0
+area = 0
+colorVol = (255, 0, 0)
+while True:
+    # For Find hand Landmarks
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList, bbox = detector.findPosition(img)
+    # Get the tip of the index and middle finger
+    if len(lmList) != 0:
+        x1, y1 = lmList[8][1:]
+        x2, y2 = lmList[12][1:]
+        # Filter based on size
+        area = (bbox[2] - bbox[0]) * (bbox[3] - bbox[1]) // 100
+        # print(area)
+        if 250 < area < 1000:
+            # Find Distance between index and Thumb
+            length, img, lineInfo = detector.findDistance(4, 8, img)
+
+            # Convert Volume
+            volBar = np.interp(length, [50, 200], [400, 150])
+            volPer = np.interp(length, [50, 200], [0, 100])
+
+            # Reduce Resolution to make it smoother
+            smoothness = 10
+            volPer = smoothness * round(volPer / smoothness)
+
+            # Check fingers up
+            fingers = detector.fingersUp()
+
+            # If pinky is down set volume
+            if not fingers[4]:
+                volume.SetMasterVolumeLevelScalar(volPer / 100, None)
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+                colorVol = (0, 255, 0)
+            else:
+                colorVol = (255, 0, 0)
+
+    # Checking which fingers are up
+    fingers = detector.fingersUp()
+
+    cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR),
+                  (255, 0, 255), 2)
+    # For Moving the cursor by detecting which finger are up
+    if fingers[1] == 1 and fingers[2] == 0:
+        # Converting Coordinates
+        x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
+        y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
+        # Smoothen Values
+        clocX = plocX + (x3 - plocX) / smoothening
+        clocY = plocY + (y3 - plocY) / smoothening
+
+        # Move Mouse by using the coordinate
+        autopy.mouse.move(wScr - clocX, clocY)
+        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+        plocX, plocY = clocX, clocY
+
+    # For clicking by detecting index finger and middle finger
+    if fingers[1] == 1 and fingers[2] == 1:
+        # Find distance between index finger and middle finger
+        length, img, lineInfo = detector.findDistance(8, 12, img)
+        # print(length)
+        """
+        Mouse will be click if distance between index finger and middle finger less than 40 
+        which mean index finger and middle finger need to be close
+        """
+        if length < 40:
+            cv2.circle(img, (lineInfo[4], lineInfo[5]),
+                       15, (0, 255, 0), cv2.FILLED)
+            autopy.mouse.click()
+
+    # Drawing the things we need like volume
+    cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
+    cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX,
+                1, (255, 0, 0), 3)
+    cVol = int(volume.GetMasterVolumeLevelScalar() * 100)
+    cv2.putText(img, f'Vol Set: {int(cVol)}', (400, 50), cv2.FONT_HERSHEY_COMPLEX,
+                1, colorVol, 3)
+
+    # To see the Frame Rate
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3,
+                (255, 0, 0), 3)
+
+    # Display or showing the app
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
